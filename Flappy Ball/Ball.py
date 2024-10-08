2b42f94f5801d121b794a2cb895a22f61159e48c
username123321246 <george.mircea.vrana@gmail.com>
2024-10-08 17:15:00 +0200

    Update Ball.py

--- Flappy Ball/Ball.py
+++ Flappy Ball/Ball.py
@@ -24,12 +24,13 @@ class Ball():
           screen.draw.filled_circle(pos, self.radius, CLR)
 
 ball = Ball(50,100)
-
+ball2 = Ball(90, 200)
 
 
 def draw():
       screen.clear()
       ball.draw()
+      ball2.draw()
 
 
 
@@ -37,6 +38,8 @@ def on_key_down(key):
      #pressing a key will kick the ball upwards.
      if key == keys.SPACE:
         ball.vy = -500
+     if key == keys.UP:
+        ball2.vy = -300
         
 def update(dt):
      # Equation of motion = v = u + at  v = final velocity, u = initial velocity, a = acceleration, t = time
@@ -52,11 +55,34 @@ def update(dt):
      ball.x = ball.x + ball.vx * dt
      if ball.x >= WIDTH - ball.radius or ball.x  <= ball.radius:
                ball.vx = -ball.vx
+     
+
+     uy = ball2.vy
+     ball2.vy = ball2.vy + GRAVITY * dt 
+     ball2.y = ball2.y + (uy + ball2.vy) * 0.5 * dt
+     # Detect and handle bounce
+     if ball2.y >= HEIGHT - ball2.radius: # We have bounced
+          ball2.y = HEIGHT - ball2.radius # Fix the position at the bottom
+          ball2.vy = -ball2.vy * 0.9 # Inelastic collision
+      # x component does not have acceleration
+
+     ball2.x = ball2.x + ball2.vx * dt
+     if ball2.x >= WIDTH - ball2.radius or ball2.x  <= ball2.radius:
+               ball2.vx = -ball2.vx
+
+
+
 
 
 
 
 pgzrun.go()
+      
+
+
+
+
+
       
 
      
